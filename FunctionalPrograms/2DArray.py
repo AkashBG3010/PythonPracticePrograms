edca807b3a6eee87b054b7efe9f929d93db21dda
Akash B G <akashbpura@gmail.com>
2022-02-10 15:10:01 +0530

    Modified existing program with addition of function method

--- FunctionalPrograms/2DArray.py
+++ FunctionalPrograms/2DArray.py
@@ -1,16 +1,18 @@
-import array as arr
+def array(rows, columns):       # Function for array operation
+    if (rows > 0) & (columns > 0):
+        print("2D-Array of", rows, "X", columns, " is: ")
+        arr = [[0] * columns] * rows
+        arr[0][0] = 0  # method to add elements
 
-rows = int(input("Enter the number of rows: \n"))
-columns = int(input("Enter the number of columns: \n"))
+        for row in arr:
+            print(row)
+    else:
+        print("Invalid input! Please enter non negative input only")        # Error Statement
 
-if (rows > 0) & (columns > 0):
-    print("2D-Array of", rows,"X",columns," is: ")
-    arr = [[0] * columns] * rows
-    arr[0][0] = 0  # method to add elements
 
-    for row in arr:
-        print(row)
-else:
-    print("Invalid input! Please enter non negative input only")
+rows = int(input("Enter the number of rows: \n"))       # Reads input from user
+columns = int(input("Enter the number of columns: \n"))     # Reads input from user
+
+array(rows, columns)        # Function Calling for array operation
 
 print("----End of Program-----")
