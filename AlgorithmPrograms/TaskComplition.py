ce6c9da81c93f9a415f39d8d6b7e2c3a59be1035
Akash B G <akashbpura@gmail.com>
2022-02-16 22:09:33 +0530

    Modified some method algorithm programs

--- AlgorithmPrograms/TaskComplition.py
+++ AlgorithmPrograms/TaskComplition.py
@@ -29,12 +29,14 @@ def binarySearch(array, number, si, ei):
 
 
 def addJob(length, array, deadline, minutes, late):
+
     if length < deadline:
         for i in range(deadline - length):
             array.append(i + length)
         length = deadline
-    minLeft = minutes
-    index = returnIndex(array, deadline - 1)
+        minLeft = minutes
+        index = returnIndex(array, deadline - 1)
+
     if index != None:
         while index >= 0 and minLeft > 0:
             array.pop(index)
@@ -50,14 +52,13 @@ def addJob(length, array, deadline, minutes, late):
 
 if __name__ == '__main__':
 
-    n = int(input().strip())
-
+    num = int(input("Enter the number: ").strip())
     time = 0
     length = 0
-
     nl = []
     late = 0
-    for op in range(n):
-        job = input().split(' ')
+
+    for op in range(num):
+        job = input("Enter the job: ").split(' ')
         late, length = addJob(length, nl, int(job[0]), int(job[1]), late)
         print(late)
