409133a221322b25839959cc09f4468bc6d8a0f0
Akash B G <akashbpura@gmail.com>
2022-02-18 21:54:00 +0530

    Added to perform some operations on csv file

---
+++ OopsPrograms/CSV/contacts_main.py
@@ -0,0 +1,27 @@
+import csv
+
+# Regular reader and writer method
+
+with open('contact.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    with open('new_contact.csv', 'w') as new_file:
+        csv_writer = csv.writer(new_file, delimiter='\t')
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
+
+
+# Dictionary reader and writer method
+#
+# with open('contact.csv', 'r') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
+#
+#     with open('new_contact.csv', 'w') as new_file:
+#         fieldnames = ['ICON_GID', 'ICON_XID', 'DESCRIPTION', 'PATH']
+#         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+#         fieldnames.writeheader()
+#
+#         for line in csv_reader:
+#             del line['DOMAIN_NAME']
+#             csv_writer.writerow(line)
