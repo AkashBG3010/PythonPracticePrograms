18fcdd14a83e6d2469a3e7f0e091461d512b72b2
Akash B G <akashbpura@gmail.com>
2022-02-19 09:09:38 +0530

    Modified with classes and function method

--- OopsPrograms/File_IO/contact_manager.py
+++ OopsPrograms/File_IO/contact_manager.py
@@ -1,13 +1,19 @@
-with open('contact.txt', 'r') as file:      # To read the file. Here no need to close
-    print("All the contents in the file: ")
-    print("------------------------------------------------------------")
-    for line in file:       # Efficient method to read
-        print(line, end='')        # printing the contents of the file
-print("\n")
-print("------------------------------------------------------------")
+def operations_file():
 
-with open('text.txt', 'w') as new_file:
-    new_file.write("Hello there!")
+    with open('contact.txt', 'r') as file:      # To read the file. Here no need to close
+        print("All the contents in the file: ")
+        print("------------------------------------------------------------")
+        for line in file:       # Efficient method to read
+            print(line, end='')        # printing the contents of the file
+    print("\n")
+    print("------------------------------------------------------------")
+
+    with open('text.txt', 'w') as new_file:
+        new_file.write("Hello there!")
+
+
+class main:
+    operations_file()
 
 # file_contents = file.read(100)     # to read first 100(size to read) characters of the file
 # print(file.tell())        # to check the current position
